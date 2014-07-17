cfd73deeb81130d64c52e3f5eb506ecde9a4e2cd
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-07-16 22:32:30 -0700

    Start a clean server for each test

--- src/test/js/test.js
+++ src/test/js/test.js
@@ -2,26 +2,47 @@ var PORT = 9090;
 
 function newServer(port) {
   var lastValue;
-  var io = require('socket.io').listen(port);
-  io.on('connection', function(socket) {
-    socket.emit('value', lastValue);
-    socket.on('set', function(value, ack) {
-      io.emit('value', value);
-      lastValue = value;
-      ack();
-    });
-  });
+  var io;
+  var http = require('http');
+  var server = http.createServer();
+  return {
+    start: function() {
+      io = require('socket.io').listen(server);
+      io.on('connection', function(socket) {
+        console.log("CONN", lastValue);
+        socket.emit('value', lastValue);
+        socket.on('set', function(value, ack) {
+          io.emit('value', value);
+          lastValue = value;
+          ack();
+        });
+      });
+      server.listen(port);
+      return this;
+    },
+    destroy: function() {
+      server.close();
+    }
+  }
 }
-
-newServer(PORT);
 
 require('./env');
 var q = require('q');
 
 describe('server', function() {
+  var server;
+
   function newClient() {
     return require('socket.io-client').connect('http://localhost:' + PORT, {'force new connection': true });
   }
+
+  beforeEach(function() {
+    server = newServer(PORT).start();
+  });
+
+  afterEach(function() {
+    server.destroy();
+  });
 
   describe('setting a value', function() {
     var client1Value;
