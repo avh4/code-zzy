80378ede8a50f417e928e6a76a348c1538c31292
Aaron VonderHaar <gruen0aermel@gmail.com>
2014-07-18 21:59:44 -0700

    Cleanup EngineTest to be more deterministic

--- src/test/js/EngineTest.js
+++ src/test/js/EngineTest.js
@@ -44,12 +44,15 @@ describe('engine', function() {
     it('keeps the values separate', function() {
       dropboxValue = newSubscribedClient('dropbox', 1);
       evernoteValue = newSubscribedClient('evernote', 1);
-      engine.set('dropbox', {username: 'bobama'});
-      engine.set('evernote', {username: 'wadama'});
-      return q.all([
-        dropboxValue.then(function(val) { expect(val).to.eql({username: 'bobama'})}),
-        evernoteValue.then(function(val) { expect(val).to.eql({username: 'wadama'})}),
-      ]);
+      return q().then(function() {
+        return engine.set('dropbox', {username: 'bobama'});
+      }).then(function() {
+        return engine.set('evernote', {username: 'wadama'});
+      }).then(function() {
+        return dropboxValue.then(function(val) { expect(val).to.eql({username: 'bobama'})});
+      }).then(function() {
+        return evernoteValue.then(function(val) { expect(val).to.eql({username: 'wadama'})});
+      });
     });
 
     it('doesn\'t blow up when setting a path that is not subscribed to', function() {
